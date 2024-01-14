f5bcd67d2cf07f6dc7bdd89c99315800ec3bd527
Carl Douglas <105003+carld@users.noreply.github.com>
2024-01-14 15:18:02 +1300

    whitespace

--- curl-test.sh
+++ curl-test.sh
@@ -2,10 +2,12 @@
 
 HOST=localhost:9000
 
-curl --request OPTIONS http://$HOST/users
-curl --request GET http://$HOST/users
-curl --request GET http://$HOST/users/031F2E84-01FB-7344-8CDD-B92FAC09007B
-curl --request POST --data "name=curl+test" http://$HOST/users
-
-echo 
+curl --silent --request OPTIONS http://$HOST/users
+echo
+curl --silent --request GET http://$HOST/users
+echo
+curl --silent --request GET http://$HOST/users/031F2E84-01FB-7344-8CDD-B92FAC09007B
+echo
+curl --silent --request POST --data "name=curl+test" http://$HOST/users
+echo
 
